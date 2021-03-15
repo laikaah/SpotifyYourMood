d8aadec177db0b4fc737080811d508ae9b5898b6
laikaah <lykayzabeldapitan@gmail.com>
2021-03-15 18:50:56 +0800

    project

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+function getSong(moodID) {
+     
+    if(moodID<0 || moodID>2){invalidInput();return;}
+    var randomNum = randomizer(0,2);
+
+    console.log(moodID +" "+ randomNum);
+    var playlistPicked = playlistList(moodID, randomNum);
+    var link = "https://open.spotify.com/playlist/"+playlistPicked;
+
+    document.getElementById("link").innerHTML = link;
+    alert("Redirecting to "+link);
+    window.location.href = link;
+}
+
+function randomizer(min, max) {
+    return Math.floor(Math.random()*(max-min)) + min;
+
+}
+
+function invalidInput() {
+    alert("Not a valid Input");
+}
---
+++ scripts/playlist.js
@@ -0,0 +1,9 @@
+function playlistList(category,id){
+    var playlists = [
+        ["0UBTU2i0FODkIgMb0uFxKM?si=V6T3AfWvRba6Afau7X3_Lw", "7zpDiEGEnUXgFCf9vWZulL?si=geVY3VfwQaSnqUyfSDsACg"], //happy
+        ["02oHZT7mb65Sj2RMcJJcjy?si=buL4TQUZSEyj7GuoOqZkRA","6KlEX6PEcSUN6iO1DiiCpb?si=HK81chwnSnuwLLUgab4uJQ"], //sad
+        ["3oV8s0U48utLFLKN6NYmev?si=U4WKaJKZSVmXkBAtKrXDMg", "0pPPtaQhVQbq1SWYzF9uMa?si=z3dIOt5yS6Oy1a508X0y7Q" ], //studious
+    ]
+
+    return playlists[category, id];
+}
