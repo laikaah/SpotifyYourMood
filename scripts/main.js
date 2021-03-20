0b846cf9b0336782c4b1e66381c9e69a74d0834f
Kair0 <kyleangelodapitan@gmaik.com>
2021-03-21 04:55:51 +0800

    fixed the layout of the webpage

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     const happy_button = document.getElementById("happy");
     const sad_button = document.getElementById("sad");
+    const love_button = document.getElementById("love");
+    const contented_button = document.getElementById("contented");
+    const stressed_button = document.getElementById("stressed");
+    const angry_button = document.getElementById("angry");
     const studious_button = document.getElementById("studious");
 
     happy_button.onclick = () => {
@@ -9,8 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
     sad_button.onclick = () => {
         getSong(1);
     };
-    studious_button.onclick = () => {
+    love_button.onclick = () => {
         getSong(2);
+    };
+    contented_button.onclick = () => {
+        getSong(3);
+    };
+    stressed_button.onclick = () => {
+        getSong(4);
+    };
+    angry_button.onclick = () => {
+        getSong(5);
+    };
+    studious_button.onclick = () => {
+        getSong(6);
     };
 });
 
@@ -25,7 +42,6 @@ function getSong(moodID) {
     var playlistPicked = playlistList(moodID, randomNum);
     var link = "https://open.spotify.com/playlist/" + playlistPicked;
 
-    document.getElementById("link").innerHTML = link;
     alert("Redirecting to " + link);
     window.location.replace(link);
 }
@@ -33,3 +49,7 @@ function getSong(moodID) {
 const randomizer = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min;
 };
+
+const getButton = () => {
+    
+}
--- scripts/playlist.js
+++ scripts/playlist.js
@@ -1,7 +1,12 @@
 function playlistList(category, id) {
+
     var playlists = [
         ["0UBTU2i0FODkIgMb0uFxKM?si=V6T3AfWvRba6Afau7X3_Lw", "7zpDiEGEnUXgFCf9vWZulL?si=geVY3VfwQaSnqUyfSDsACg"], //happy
         ["02oHZT7mb65Sj2RMcJJcjy?si=buL4TQUZSEyj7GuoOqZkRA", "6KlEX6PEcSUN6iO1DiiCpb?si=HK81chwnSnuwLLUgab4uJQ"], //sad
+        ["3oV8s0U48utLFLKN6NYmev?si=U4WKaJKZSVmXkBAtKrXDMg", "0pPPtaQhVQbq1SWYzF9uMa?si=z3dIOt5yS6Oy1a508X0y7Q"], //love
+        ["3oV8s0U48utLFLKN6NYmev?si=U4WKaJKZSVmXkBAtKrXDMg", "0pPPtaQhVQbq1SWYzF9uMa?si=z3dIOt5yS6Oy1a508X0y7Q"], //contented
+        ["3oV8s0U48utLFLKN6NYmev?si=U4WKaJKZSVmXkBAtKrXDMg", "0pPPtaQhVQbq1SWYzF9uMa?si=z3dIOt5yS6Oy1a508X0y7Q"], //stressed
+        ["3oV8s0U48utLFLKN6NYmev?si=U4WKaJKZSVmXkBAtKrXDMg", "0pPPtaQhVQbq1SWYzF9uMa?si=z3dIOt5yS6Oy1a508X0y7Q"], //angry
         ["3oV8s0U48utLFLKN6NYmev?si=U4WKaJKZSVmXkBAtKrXDMg", "0pPPtaQhVQbq1SWYzF9uMa?si=z3dIOt5yS6Oy1a508X0y7Q"], //studious
     ]
 
@@ -11,3 +16,4 @@ function playlistList(category, id) {
 function anotherFunctionFromFrancis() {
     console.log('This is another function')
 }
+
